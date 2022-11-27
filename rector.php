e829ba47fb3fed51c81f5cc312efd914019b8a6e
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2022-11-27 14:44:15 +0100

    Updated rules

--- rector.php
+++ rector.php
@@ -5,37 +5,43 @@ declare(strict_types = 1);
 use Rector\CodeQuality\Rector\Array_\CallableThisArrayToAnonymousFunctionRector;
 use Rector\CodeQuality\Rector\If_\ShortenElseIfRector;
 use Rector\CodeQuality\Rector\Include_\AbsolutizeRequireAndIncludePathRector;
-use Rector\Core\Configuration\Option;
+use Rector\Config\RectorConfig;
 use Rector\Core\ValueObject\PhpVersion;
 use Rector\Set\ValueObject\SetList;
+use Rector\Symfony\Set\SensiolabsSetList;
 use Rector\Symfony\Set\SymfonySetList;
 use Rector\Symfony\Set\TwigSetList;
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
-return static function (ContainerConfigurator $containerConfigurator): void
+return static function (RectorConfig $rectorConfig): void
 {
-    // get parameters
-    $parameters = $containerConfigurator->parameters();
-
-    $parameters->set(Option::PATHS, [
+    $rectorConfig->paths([
         __DIR__.'/src',
         __DIR__.'/tests',
     ]);
-    $parameters->set(Option::PHP_VERSION_FEATURES, PhpVersion::PHP_74);
 
-    $containerConfigurator->import(SetList::DEAD_CODE);
-    $containerConfigurator->import(SetList::CODE_QUALITY);
-    $containerConfigurator->import(SetList::PHP_74);
-    $containerConfigurator->import(SetList::PHP_80);
-    $containerConfigurator->import(SetList::PHP_81);
+    $rectorConfig->phpVersion(PhpVersion::PHP_74);
+    $rectorConfig->importNames(true, false);
 
-    //-- Symfony Framework
-    $containerConfigurator->import(SymfonySetList::SYMFONY_44);
-    $containerConfigurator->import(TwigSetList::TWIG_240);
+    $rectorConfig->import(SetList::DEAD_CODE);
+    $rectorConfig->import(SetList::CODE_QUALITY);
+    $rectorConfig->import(SetList::PHP_74);
+    $rectorConfig->import(SetList::PHP_80);
+    $rectorConfig->import(SetList::PHP_81);
 
-    //-- Skip some rules/files ...
-    $parameters->set(Option::SKIP, [
-        // __DIR__.'/src/core/Twig/NodeVisitor',
+    // -- Symfony Framework
+    $rectorConfig->import(SymfonySetList::SYMFONY_40);
+    $rectorConfig->import(SymfonySetList::SYMFONY_41);
+    $rectorConfig->import(SymfonySetList::SYMFONY_42);
+    $rectorConfig->import(SymfonySetList::SYMFONY_43);
+    $rectorConfig->import(SymfonySetList::SYMFONY_44);
+    $rectorConfig->import(SymfonySetList::SYMFONY_CODE_QUALITY);
+    $rectorConfig->import(SymfonySetList::SYMFONY_STRICT);
+    $rectorConfig->import(SymfonySetList::SYMFONY_CONSTRUCTOR_INJECTION);
+    $rectorConfig->import(TwigSetList::TWIG_240);
+    $rectorConfig->import(TwigSetList::TWIG_UNDERSCORE_TO_NAMESPACE);
+
+    // -- Skip some rules/files ...
+    $rectorConfig->skip([
         ShortenElseIfRector::class,
         CallableThisArrayToAnonymousFunctionRector::class,
         AbsolutizeRequireAndIncludePathRector::class,
