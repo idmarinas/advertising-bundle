b111e0d905bbe67c10b6ad56a941bb907de79664
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2022-12-27 11:10:50 +0100

    Updated rules

--- rector.php
+++ rector.php
@@ -7,8 +7,9 @@ use Rector\CodeQuality\Rector\If_\ShortenElseIfRector;
 use Rector\CodeQuality\Rector\Include_\AbsolutizeRequireAndIncludePathRector;
 use Rector\Config\RectorConfig;
 use Rector\Core\ValueObject\PhpVersion;
+use Rector\Set\ValueObject\LevelSetList;
 use Rector\Set\ValueObject\SetList;
-use Rector\Symfony\Set\SensiolabsSetList;
+use Rector\Symfony\Set\SymfonyLevelSetList;
 use Rector\Symfony\Set\SymfonySetList;
 use Rector\Symfony\Set\TwigSetList;
 
@@ -24,16 +25,12 @@ return static function (RectorConfig $rectorConfig): void
 
     $rectorConfig->import(SetList::DEAD_CODE);
     $rectorConfig->import(SetList::CODE_QUALITY);
-    $rectorConfig->import(SetList::PHP_74);
+    $rectorConfig->import(LevelSetList::UP_TO_PHP_74);
     $rectorConfig->import(SetList::PHP_80);
     $rectorConfig->import(SetList::PHP_81);
 
     // -- Symfony Framework
-    $rectorConfig->import(SymfonySetList::SYMFONY_40);
-    $rectorConfig->import(SymfonySetList::SYMFONY_41);
-    $rectorConfig->import(SymfonySetList::SYMFONY_42);
-    $rectorConfig->import(SymfonySetList::SYMFONY_43);
-    $rectorConfig->import(SymfonySetList::SYMFONY_44);
+    $rectorConfig->import(SymfonyLevelSetList::UP_TO_SYMFONY_44);
     $rectorConfig->import(SymfonySetList::SYMFONY_CODE_QUALITY);
     $rectorConfig->import(SymfonySetList::SYMFONY_STRICT);
     $rectorConfig->import(SymfonySetList::SYMFONY_CONSTRUCTOR_INJECTION);
