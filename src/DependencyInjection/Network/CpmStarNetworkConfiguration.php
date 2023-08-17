6ad5b1f29f10d32ada77a982b879e7fdda0f5ced
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2023-08-17 16:52:54 +0200

    First commit

---
+++ src/DependencyInjection/Network/CpmStarNetworkConfiguration.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * This file is part of Bundle "IDM Advertising Bundle".
+ *
+ * @see https://github.com/idmarinas/advertising-bundle
+ *
+ * @license https://github.com/idmarinas/advertising-bundle/blob/master/LICENSE.txt
+ * @author IDMarinas
+ *
+ * @since 0.1.0
+ */
+
+namespace Idm\Bundle\AdvertisingBundle\DependencyInjection\Network;
+
+use Symfony\Component\Config\Definition\Builder\NodeBuilder;
+
+class AdsenseNetworkConfiguration implements NetworkConfigurationInterface
+{
+    public function buildConfiguration(NodeBuilder $node)
+    {
+        $node
+            ->scalarNode('service_network')
+                ->defaultValue('idm_advertising.network.cpmstar')
+                ->info('Custom service network, ID of service')
+            ->end()
+            ->arrayNode('banners')
+                ->isRequired()
+                ->useAttributeAsKey('name')
+                ->arrayPrototype()
+                    ->children()
+                        ->integerNode('cpmstar_pid')
+                            ->min(0)
+                            ->isRequired()
+                            ->info('Pool ID of Ad block 8XXXXX1')
+                        ->end()
+                    ->end()
+                ->end()
+            ->end()
+        ;
+    }
+}
---
+++ src/Provider/Network/CpmStarNetwork.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * This file is part of Bundle "IDM Advertising Bundle".
+ *
+ * @see https://github.com/idmarinas/advertising-bundle
+ *
+ * @license https://github.com/idmarinas/advertising-bundle/blob/master/LICENSE.txt
+ * @author IDMarinas
+ *
+ * @since 1.6.0
+ */
+
+namespace Idm\Bundle\AdvertisingBundle\Provider\Network;
+
+use Idm\Bundle\AdvertisingBundle\Provider\NetworkAbstract;
+
+final class CpmStarNetwork extends NetworkAbstract
+{
+
+    public function getBanner(string $slot): string
+    {
+        $config     = $this->getConfig();
+        $slotConfig = $config['banners'][$slot] ?? [];
+
+        return sprintf('<script language="Javascript"
+            src="//server.cpmstar.com/view.aspx?poolid=%1$s&script=1&rnd=%2$s"></script>',
+            $slotConfig['cpmstar_pid'] ?? 0,
+            random_int(100000, 999999)
+        );
+    }
+
+    public function getScriptUrl(): string
+    {
+        return '';
+    }
+}
+
+
+// <SCRIPT language="Javascript">
+// var cpmstar_rnd=Math.round(Math.random()*999999);
+// var cpmstar_pid=54963;
+// document.writeln("<SCR"+"IPT language='Javascript' src='//server.cpmstar.com/view.aspx?poolid="+cpmstar_pid+"&script=1&rnd="+cpmstar_rnd+"'></SCR"+"IPT>");
+// </SCRIPT>
