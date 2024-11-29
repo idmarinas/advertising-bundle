0413fdbb0d24162d63ca4843593ab43caa17f8ac
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2024-11-29 19:04:47 +0100

    Updated for Recto 1.2

--- rector.php
+++ rector.php
@@ -1,47 +1,26 @@
 <?php
 
-declare(strict_types = 1);
+declare(strict_types=1);
 
-use Rector\CodeQuality\Rector\Array_\CallableThisArrayToAnonymousFunctionRector;
-use Rector\CodeQuality\Rector\If_\ShortenElseIfRector;
-use Rector\CodeQuality\Rector\Include_\AbsolutizeRequireAndIncludePathRector;
 use Rector\Config\RectorConfig;
-use Rector\Core\ValueObject\PhpVersion;
-use Rector\Set\ValueObject\LevelSetList;
-use Rector\Set\ValueObject\SetList;
-use Rector\Symfony\Set\SymfonyLevelSetList;
 use Rector\Symfony\Set\SymfonySetList;
-use Rector\Symfony\Set\TwigSetList;
+use Rector\TypeDeclaration\Rector\ClassMethod\AddVoidReturnTypeWhereNoReturnRector;
 
-return static function (RectorConfig $rectorConfig): void
-{
-    $rectorConfig->paths([
-        __DIR__.'/src',
-        __DIR__.'/tests',
-    ]);
-
-    $rectorConfig->phpVersion(PhpVersion::PHP_74);
-    $rectorConfig->importNames(true, false);
-
-    $rectorConfig->import(SetList::DEAD_CODE);
-    $rectorConfig->import(SetList::CODE_QUALITY);
-    $rectorConfig->import(LevelSetList::UP_TO_PHP_74);
-    $rectorConfig->import(SetList::PHP_80);
-    $rectorConfig->import(SetList::PHP_81);
-    $rectorConfig->import(SetList::PHP_82);
-
-    // -- Symfony Framework
-    $rectorConfig->import(SymfonyLevelSetList::UP_TO_SYMFONY_44);
-    $rectorConfig->import(SymfonySetList::SYMFONY_CODE_QUALITY);
-    $rectorConfig->import(SymfonySetList::SYMFONY_STRICT);
-    $rectorConfig->import(SymfonySetList::SYMFONY_CONSTRUCTOR_INJECTION);
-    $rectorConfig->import(TwigSetList::TWIG_240);
-    $rectorConfig->import(TwigSetList::TWIG_UNDERSCORE_TO_NAMESPACE);
-
-    // -- Skip some rules/files ...
-    $rectorConfig->skip([
-        ShortenElseIfRector::class,
-        CallableThisArrayToAnonymousFunctionRector::class,
-        AbsolutizeRequireAndIncludePathRector::class,
-    ]);
-};
+return RectorConfig::configure()
+                   ->withPaths([
+	                   __DIR__ . '/src',
+	                   __DIR__ . '/tests',
+                   ])
+                   ->withPhpSets(php80: true)
+                   ->withPreparedSets(
+	                   deadCode          : true, codeQuality: true, codingStyle: true, doctrineCodeQuality: true,
+	                   symfonyCodeQuality: true, symfonyConfigs: true, twig: true
+                   )
+                   ->withImportNames(removeUnusedImports: true)
+                   ->withTypeCoverageLevel(0)
+                   ->withSets([
+	                   SymfonySetList::SYMFONY_54,
+	                   SymfonySetList::SYMFONY_CONSTRUCTOR_INJECTION,
+                   ])
+                   ->withRules([AddVoidReturnTypeWhereNoReturnRector::class])
+;
