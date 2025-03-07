0b5e948931d4bdbd8e1406dd85597dbeac82e940
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-03-07 16:50:46 +0100

    refactor(rector): update configuration for Rector with new paths and sets
    - Changed paths to include new directories: app/src, factories, and fixtures.
    - Updated PHP version set to 8.2.
    - Adjusted Symfony sets to use Symfony 6.4.
    - Removed specific rules and skip configurations for cleaner setup.

--- rector.php
+++ rector.php
@@ -1,26 +1,50 @@
 <?php
+/**
+ * Copyright 2022-2025 (C) IDMarinas - All Rights Reserved
+ *
+ * Last modified by "IDMarinas" on 07/03/2025, 16:50
+ *
+ * @project IDMarinas Advertising Bundle
+ * @see     https://github.com/idmarinas/advertising-bundle
+ *
+ * @file    rector.php
+ * @date    10/01/2022
+ * @time    12:51
+ *
+ * @author  Iván Diaz Marinas (IDMarinas)
+ * @license BSD 3-Clause License
+ *
+ * @since   0.1.0
+ */
 
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
 use Rector\Symfony\Set\SymfonySetList;
-use Rector\TypeDeclaration\Rector\ClassMethod\AddVoidReturnTypeWhereNoReturnRector;
 
 return RectorConfig::configure()
-                   ->withPaths([
-	                   __DIR__ . '/src',
-	                   __DIR__ . '/tests',
-                   ])
-                   ->withPhpSets(php80: true)
-                   ->withPreparedSets(
-	                   deadCode          : true, codeQuality: true, codingStyle: true, doctrineCodeQuality: true,
-	                   symfonyCodeQuality: true, symfonyConfigs: true, twig: true
-                   )
-                   ->withImportNames(removeUnusedImports: true)
-                   ->withTypeCoverageLevel(0)
-                   ->withSets([
-	                   SymfonySetList::SYMFONY_54,
-	                   SymfonySetList::SYMFONY_CONSTRUCTOR_INJECTION,
-                   ])
-                   ->withRules([AddVoidReturnTypeWhereNoReturnRector::class])
+	->withPaths([
+		__DIR__ . '/app/src',
+		__DIR__ . '/factories',
+		__DIR__ . '/fixtures',
+		__DIR__ . '/src',
+		__DIR__ . '/tests',
+	])
+	->withPhpSets(php82: true)
+	->withPreparedSets(
+		deadCode           : true,
+		codeQuality        : true,
+		codingStyle        : true,
+		doctrineCodeQuality: true,
+		symfonyCodeQuality : true,
+		symfonyConfigs     : true,
+		twig               : true
+	)
+	->withImportNames(removeUnusedImports: true)
+	->withTypeCoverageLevel(0)
+	->withSets([
+		SymfonySetList::SYMFONY_64,
+	])
+	->withRules([])
+	->withSkip([])
 ;
