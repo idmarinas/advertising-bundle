c77bb294f4e08b42ff927fb4dd61d86ab365231c
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-03-09 19:21:53 +0100

    feat(bundle): add configuration method to IdmAdvertisingBundle
    - Added a new method `configure` to the `IdmAdvertisingBundle` class.
    - This method imports definitions from the specified configuration file.

--- src/IdmAdvertisingBundle.php
+++ src/IdmAdvertisingBundle.php
@@ -19,13 +19,22 @@
 
 namespace Idm\Bundle\Advertising;
 
+use Idm\Bundle\Advertising\Provider\Network\NetworkInterface;
+use InvalidArgumentException;
+use Symfony\Component\Config\Definition\Configurator\DefinitionConfigurator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\DependencyInjection\Reference;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 use Symfony\UX\TwigComponent\TwigComponentBundle;
 
 final class IdmAdvertisingBundle extends AbstractBundle
 {
+	public function configure (DefinitionConfigurator $definition): void
+	{
+		$definition->import(dirname(__DIR__) . '/config/definitions.php');
+	}
+
 	public function loadExtension (array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
 	{
 		$container->import(dirname(__DIR__) . '/config/services.php');
