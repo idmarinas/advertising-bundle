65c4772424613cb911223c3961650aba074e044e
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-03-11 20:00:30 +0100

    feat(twig): add ScriptsRuntime class for managing advertising scripts
    - Implemented a new class `ScriptsRuntime` that provides methods to
      show and render advertising scripts based on the network name.
    - Utilizes the event dispatcher to manage script URLs and handle events
      related to Twig scripts.

---
+++ src/Twig/Runtime/ScriptsRuntime.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Copyright 2025 (C) IDMarinas - All Rights Reserved
+ *
+ * Last modified by "IDMarinas" on 11/03/2025, 19:42
+ *
+ * @project IDMarinas Advertising Bundle
+ * @see     https://github.com/idmarinas/advertising-bundle
+ *
+ * @file    ScriptsRuntime.php
+ * @date    07/03/2025
+ * @time    17:32
+ *
+ * @author  Iván Diaz Marinas (IDMarinas)
+ * @license BSD 3-Clause License
+ *
+ * @since   2.0.0
+ */
+
+namespace Idm\Bundle\Advertising\Twig\Runtime;
+
+use Idm\Bundle\Advertising\Event\TwigScriptsEvent;
+use Idm\Bundle\Advertising\Provider\ProviderHub;
+use Symfony\Contracts\EventDispatcher\EventDispatcherInterface;
+use Twig\Extension\RuntimeExtensionInterface;
+use function sprintf;
+
+final readonly class ScriptsRuntime implements RuntimeExtensionInterface
+{
+	public function __construct (private EventDispatcherInterface $eventDispatcher, private ProviderHub $providerHub) {}
+
+	public function showScripts (?string $networkName = null): string
+	{
+		if (!$this->providerHub->isAdvertisingEnabled()) {
+			return '';
+		}
+
+		$scripts = $this->providerHub->getScriptsUrls($networkName);
+
+		$event = new TwigScriptsEvent();
+		$event->setScripts($scripts);
+		$this->eventDispatcher->dispatch($event, TwigScriptsEvent::TWIG_SHOW_SCRIPTS_POST);
+
+		$scripts = $event->getScripts();
+
+		if ([] == $scripts) {
+			return '';
+		}
+
+		return sprintf(
+			'<script%1$s async crossorigin="anonymous" src="'
+			. implode('"></script><script%1$s async crossorigin="anonymous" src="', $scripts) .
+			'"></script>',
+			$event->getNonceAttribute()
+		);
+	}
+
+	public function render (array $args = []): string
+	{
+		$network = $args['network'] ?? null;
+
+		return $this->showScripts($network);
+	}
+}
