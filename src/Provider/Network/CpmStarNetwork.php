bfb0b85b89f9109de02adbef1efd53c70a3bcf4f
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-03-11 21:21:57 +0100

    feat(CpmStarNetwork): refactor getBanner and configureBanners methods
    - Updated the getBanner method to use the NetworkEvent for banner
      retrieval.
    - Refactored configureBanners to initialize banners with
      ArrayObject and denormalize configuration.
    - Improved code readability and structure.

--- src/Provider/Network/CpmStarNetwork.php
+++ src/Provider/Network/CpmStarNetwork.php
@@ -1,47 +1,65 @@
 <?php
 
 /**
- * This file is part of Bundle "IDM Advertising Bundle".
+ * Copyright 2023-2025 (C) IDMarinas - All Rights Reserved
  *
- * @see https://github.com/idmarinas/advertising-bundle
+ * Last modified by "IDMarinas" on 11/03/2025, 21:14
  *
- * @license https://github.com/idmarinas/advertising-bundle/blob/master/LICENSE.txt
- * @author IDMarinas
+ * @project IDMarinas Advertising Bundle
+ * @see     https://github.com/idmarinas/advertising-bundle
  *
- * @since 1.6.0
+ * @file    CpmStarNetwork.php
+ * @date    17/08/2023
+ * @time    16:52
+ *
+ * @author  Iván Diaz Marinas (IDMarinas)
+ * @license BSD 3-Clause License
+ *
+ * @since   1.6.0
  */
 
 namespace Idm\Bundle\Advertising\Provider\Network;
 
-use Idm\Bundle\Advertising\Provider\NetworkAbstract;
+use ArrayObject;
+use Idm\Bundle\Advertising\Event\NetworkEvent;
+use Idm\Bundle\Advertising\Provider\Banner\AbstractBanner;
+use Symfony\Component\Serializer\Exception\ExceptionInterface;
 
-final class CpmStarNetwork extends NetworkAbstract
+final class CpmStarNetwork extends AbstractNetwork
 {
+	public function getBanner (string $bannerName): ?AbstractBanner
+	{
+		if (!$this->isNetworkEnabled()) {
+			return null;
+		}
 
-    public function getBanner(string $slot): string
-    {
-        $config     = $this->getConfig();
-        $slotConfig = $config['banners'][$slot] ?? [];
+		$banner = parent::getBanner($bannerName);
 
-        if (empty($slotConfig) || ! $slotConfig['cpmstar_pid'])
-        {
-            return '';
-        }
+		$event = new NetworkEvent();
+		$event->setBanner($banner);
+		$this->eventDispatcher->dispatch($event, NetworkEvent::NETWORK_GET_BANNER_POST);
 
-        // -- If not enable return empty string.
-        if ( ! $this->isNetworkEnabled())
-        {
-            return '';
-        }
+		return $event->getBanner();
+	}
 
-        return sprintf('<script src="//server.cpmstar.com/view.aspx?poolid=%1$s&script=1&rnd=%2$s"></script>',
-            $slotConfig['cpmstar_pid'] ?? 0,
-            random_int(100000, 999999)
-        );
-    }
+	/**
+	 * @throws ExceptionInterface
+	 */
+	public function configureBanners (array $banners): NetworkInterface
+	{
+		$this->banners = new ArrayObject();
 
-    public function getScriptUrl(): string
-    {
-        return '';
-    }
+		foreach ($banners as $banner => $config) {
+			$config['name'] = $banner;
+			$obj = $this->denormalizer->denormalize($config, CpmStarNetwork::class, 'array');
+			$this->banners->offsetSet($banner, $obj);
+		}
+
+		return $this;
+	}
+
+	public function getScriptUrl (string $type): string
+	{
+		return '';
+	}
 }
