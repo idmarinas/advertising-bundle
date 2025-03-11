07b4f07a3a8248787680b2e81a644a0da865a769
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-03-11 13:35:45 +0100

    feat: enhance IdmAdvertisingBundle with compiler pass and configuration
    - Implemented CompilerPassInterface in IdmAdvertisingBundle class.
    - Added build method to register compiler pass.
    - Introduced process method to manage advertising network configurations.
    - Updated loadExtension method to import additional configuration files.
    - Added error handling for network interface implementation.

--- src/IdmAdvertisingBundle.php
+++ src/IdmAdvertisingBundle.php
@@ -2,7 +2,7 @@
 /**
  * Copyright 2021-2025 (C) IDMarinas - All Rights Reserved
  *
- * Last modified by "IDMarinas" on 07/03/2025, 19:06
+ * Last modified by "IDMarinas" on 11/03/2025, 13:19
  *
  * @project IDMarinas Advertising Bundle
  * @see     https://github.com/idmarinas/advertising-bundle
@@ -22,14 +22,21 @@ namespace Idm\Bundle\Advertising;
 use Idm\Bundle\Advertising\Provider\Network\NetworkInterface;
 use InvalidArgumentException;
 use Symfony\Component\Config\Definition\Configurator\DefinitionConfigurator;
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
-use Symfony\Component\DependencyInjection\Reference;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 use Symfony\UX\TwigComponent\TwigComponentBundle;
 
-final class IdmAdvertisingBundle extends AbstractBundle
+final class IdmAdvertisingBundle extends AbstractBundle implements CompilerPassInterface
 {
+	private array $extensionConfig;
+
+	public function build (ContainerBuilder $container): void
+	{
+		$container->addCompilerPass($this);
+	}
+
 	public function configure (DefinitionConfigurator $definition): void
 	{
 		$definition->import(dirname(__DIR__) . '/config/definitions.php');
@@ -37,13 +44,49 @@ final class IdmAdvertisingBundle extends AbstractBundle
 
 	public function loadExtension (array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
 	{
+		$this->extensionConfig = $config;
 		$container->import(dirname(__DIR__) . '/config/services.php');
+		$container->import(dirname(__DIR__) . '/config/twig.php');
 	}
 
 	public function prependExtension (ContainerConfigurator $container, ContainerBuilder $builder): void
 	{
 		if ($builder::willBeAvailable('symfony/ux-twig-component', TwigComponentBundle::class, ['symfony/twig-bundle'])) {
 			$container->import(dirname(__DIR__) . '/config/twig_component.php');
+		}
+	}
+
+	public function process (ContainerBuilder $container): void
+	{
+		$providerHub = $container->findDefinition('.idm_advertising.provider_hub');
+		$providerHub->addMethodCall('disableAdvertising'); // Disabled by default
+
+		foreach ($this->extensionConfig as $network => $settings) {
+			if ('adsense' !== $network) {
+				continue;
+			}
+
+			$definition = $container->findDefinition($settings['service_network']);
+			$class = $definition->getClass();
+
+			if (!is_subclass_of($class, NetworkInterface::class)) {
+				throw new InvalidArgumentException(
+					sprintf(
+						'Somehow the "%s" network is not implement the interface %s.',
+						$settings['service_network'],
+						NetworkInterface::class
+					)
+				);
+			}
+
+			$definition->addMethodCall('configure', [$settings]);
+			$definition->addMethodCall('disableNetwork'); // Disabled by default
+
+			if ($settings['enabled']) {
+				$providerHub->addMethodCall('enableAdvertising');
+				$definition->addMethodCall('enableNetwork');
+			}
+			$providerHub->addMethodCall('setNetwork', [$network, $definition]);
 		}
 	}
 }
