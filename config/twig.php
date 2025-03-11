b3bbc7f37406c24106d646c1fd0476e37ee767ae
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-03-11 13:40:32 +0100

    refactor(twig): replace old twig configuration with new implementation
    - Deleted the old `twig.php` configuration file in `src/Resources/config`.
    - Added a new `twig.php` configuration file in `config/` with updated
      service definitions for `AdvertisingExtension` and `AdvertisingRuntime`.

---
+++ config/twig.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Copyright 2021-2025 (C) IDMarinas - All Rights Reserved
+ *
+ * Last modified by "IDMarinas" on 11/03/2025, 13:26
+ *
+ * @project IDMarinas Advertising Bundle
+ * @see     https://github.com/idmarinas/advertising-bundle
+ *
+ * @file    twig.php
+ * @date    13/02/2021
+ * @time    17:09
+ *
+ * @author  Iván Diaz Marinas (IDMarinas)
+ * @license BSD 3-Clause License
+ *
+ * @since   0.1.0
+ */
+
+namespace Symfony\Component\DependencyInjection\Loader\Configurator;
+
+use Idm\Bundle\Advertising\Twig\Extension\AdvertisingExtension;
+use Idm\Bundle\Advertising\Twig\Runtime\AdvertisingRuntime;
+
+return static function (ContainerConfigurator $container) {
+	// @formatter:off
+	$container
+		->services()
+			->set(AdvertisingExtension::class)
+				->tag('twig.extension')
+			->set(AdvertisingRuntime::class)
+				->arg('$eventDispatcher', service('event_dispatcher'))
+				->arg('$providerHub', service('.idm_advertising.provider_hub'))
+				->tag('twig.runtime')
+				->tag('ux.twig_component.twig_renderer', ['key' => 'idmadvertising:show:banner'])
+
+	;
+	// @formatter:on
+};
