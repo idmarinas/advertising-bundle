c9be7814d7186da74db577be2e4c88c870a3b97c
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-03-11 13:30:59 +0100

    refactor(provider): replace NetworkInterface with a new implementation
    - Removed the old NetworkInterface from src/Provider.
    - Added a new NetworkInterface in src/Provider/Network with updated methods.

---
+++ src/Provider/Network/NetworkInterface.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Copyright 2021-2025 (C) IDMarinas - All Rights Reserved
+ *
+ * Last modified by "IDMarinas" on 10/03/2025, 23:55
+ *
+ * @project IDMarinas Advertising Bundle
+ * @see     https://github.com/idmarinas/advertising-bundle
+ *
+ * @file    NetworkInterface.php
+ * @date    13/02/2021
+ * @time    17:09
+ *
+ * @author  Iván Diaz Marinas (IDMarinas)
+ * @license BSD 3-Clause License
+ *
+ * @since   0.1.0
+ */
+
+namespace Idm\Bundle\Advertising\Provider\Network;
+
+use Idm\Bundle\Advertising\Provider\Banner\AbstractBanner;
+
+interface NetworkInterface
+{
+	public function getBanner (string $bannerNameName): ?AbstractBanner;
+
+	public function getScriptUrl (string $type): string;
+
+	/**
+	 * Configure network
+	 */
+	public function configure (array $config): self;
+
+	/**
+	 * Configure banners of network
+	 */
+	public function configureBanners (array $banners): self;
+
+	/**
+	 * Network is enable only if Network is enabled and Advertising is enabled
+	 */
+	public function isNetworkEnabled (): bool;
+
+	public function enableNetwork (): self;
+
+	public function disableNetwork (): self;
+}
