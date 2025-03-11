0b3f81ebe1f7f6d3be81d550b97e213fce5c3bda
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-03-11 14:27:17 +0100

    feat(services): add initial services configuration file

---
+++ app/config/services.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Copyright 2025 (C) IDMarinas - All Rights Reserved
+ *
+ * Last modified by "IDMarinas" on 11/03/2025, 14:26
+ *
+ * @project IDMarinas Advertising Bundle
+ * @see     https://github.com/idmarinas/advertising-bundle
+ *
+ * @file    services.php
+ * @date    07/03/2025
+ * @time    15:31
+ *
+ * @author  Iván Diaz Marinas (IDMarinas)
+ * @license BSD 3-Clause License
+ *
+ * @since   2.0.0
+ */
+
+namespace Symfony\Component\DependencyInjection\Loader\Configurator;
+
+return static function (ContainerConfigurator $container) {
+	// @formatter:off
+	$container
+		->services()
+	;
+	// @formatter:on
+};
