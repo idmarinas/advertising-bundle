da99a8cae3a2b9bf7494045e17d5ef2f9dbc5b2f
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-03-11 19:55:59 +0100

    feat(provider): add Adsense, Generic, and CpmStar traits
    - Introduced `AdsenseTrait` for managing Adsense network interactions.
    - Added `GenericTrait` to handle generic network functionalities.
    - Implemented `CpmStarTrait` for working with CpmStar network.
    
    These traits provide methods to get and set their respective network instances.

---
+++ src/Provider/ProviderHub/AdsenseTrait.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Copyright 2025 (C) IDMarinas - All Rights Reserved
+ *
+ * Last modified by "IDMarinas" on 08/03/2025, 13:05
+ *
+ * @project IDMarinas Advertising Bundle
+ * @see     https://github.com/idmarinas/advertising-bundle
+ *
+ * @file    AdsenseTrait.php
+ * @date    08/03/2025
+ * @time    12:56
+ *
+ * @author  Iván Diaz Marinas (IDMarinas)
+ * @license BSD 3-Clause License
+ *
+ * @since   2.0.0
+ */
+
+namespace Idm\Bundle\Advertising\Provider\ProviderHub;
+
+use Idm\Bundle\Advertising\Provider\Network\AdsenseNetwork;
+
+trait AdsenseTrait
+{
+	private AdsenseNetwork $adsenseNetwork;
+
+	public function getAdsenseNetwork (): AdsenseNetwork
+	{
+		return $this->adsenseNetwork;
+	}
+
+	public function setAdsenseNetwork (AdsenseNetwork $network): self
+	{
+		$this->adsenseNetwork = $network;
+
+		return $this;
+	}
+}
---
+++ src/Provider/ProviderHub/CpmStarTrait.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Copyright 2025 (C) IDMarinas - All Rights Reserved
+ *
+ * Last modified by "IDMarinas" on 08/03/2025, 13:06
+ *
+ * @project IDMarinas Advertising Bundle
+ * @see     https://github.com/idmarinas/advertising-bundle
+ *
+ * @file    CpmStarTrait.php
+ * @date    08/03/2025
+ * @time    12:57
+ *
+ * @author  Iván Diaz Marinas (IDMarinas)
+ * @license BSD 3-Clause License
+ *
+ * @since   2.0.0
+ */
+
+namespace Idm\Bundle\Advertising\Provider\ProviderHub;
+
+use Idm\Bundle\Advertising\Provider\Network\CpmStarNetwork;
+
+trait CpmStarTrait
+{
+	private CpmStarNetwork $cpmstarNetwork;
+
+	public function getCpmstarNetwork (): CpmStarNetwork
+	{
+		return $this->cpmstarNetwork;
+	}
+
+	public function setCpmstarNetwork (CpmStarNetwork $network): self
+	{
+		$this->cpmstarNetwork = $network;
+
+		return $this;
+	}
+}
