1bd1a9d7b8bcee1024b069d55fb6e659a9fcd278
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-03-13 20:32:35 +0100

    refactor(tests): update path for extra config in ScriptsTest.php
    - Changed the path for the extra config file in the
      ScriptsTest.php test to use dirname(__DIR__, 2) instead
      of dirname(__DIR__). This improves the directory
      structure reference and ensures correct file loading.

--- tests/Twig/Component/ScriptsTest.php
+++ tests/Twig/Component/ScriptsTest.php
@@ -2,7 +2,7 @@
 /**
  * Copyright 2025 (C) IDMarinas - All Rights Reserved
  *
- * Last modified by "IDMarinas" on 13/03/2025, 13:00
+ * Last modified by "IDMarinas" on 13/03/2025, 19:54
  *
  * @project IDMarinas Advertising Bundle
  * @see     https://github.com/idmarinas/advertising-bundle
@@ -42,7 +42,7 @@ class ScriptsTest extends KernelTestCase
 	{
 		$options = $options + [
 				'config' => static function (Kernel $kernel) {
-					$kernel->addExtraConfig(dirname(__DIR__) . '/Extension/idm_advertising.php');
+					$kernel->addExtraConfig(dirname(__DIR__, 2) . '/config/idm_advertising.php');
 				},
 			];
 
