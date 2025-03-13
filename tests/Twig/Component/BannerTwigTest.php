a78b62a0b6563f93739700316802af113b14478c
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-03-13 18:51:53 +0100

    test: add BannerTwigTest for Twig component rendering
    - Introduced a new test class `BannerTwigTest` to validate the
      rendering of the Twig component for displaying banners.
    - The test checks the output of the Twig template against the
      expected HTML structure for the AdSense banner.

---
+++ tests/Twig/Component/BannerTwigTest.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Copyright 2025 (C) IDMarinas - All Rights Reserved
+ *
+ * Last modified by "IDMarinas" on 13/03/2025, 18:45
+ *
+ * @project IDMarinas Advertising Bundle
+ * @see     https://github.com/idmarinas/advertising-bundle
+ *
+ * @file    BannerTwigTest.php
+ * @date    12/03/2025
+ * @time    16:00
+ *
+ * @author  Iván Diaz Marinas (IDMarinas)
+ * @license BSD 3-Clause License
+ *
+ * @since   2.0.0
+ */
+
+namespace Idm\Bundle\Advertising\Tests\Twig\Component;
+
+use App\Kernel;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Component\HttpKernel\KernelInterface;
+use Twig\Environment;
+
+class BannerTwigTest extends KernelTestCase
+{
+	public static function setUpBeforeClass (): void {}
+
+	protected static function createKernel (array $options = []): KernelInterface
+	{
+		/** @var Kernel $kernel */
+		$kernel = parent::createKernel($options);
+		$kernel->handleOptions($options);
+
+		return $kernel;
+	}
+
+	protected static function bootKernel (array $options = []): KernelInterface
+	{
+		$options = $options + [
+				'environment' => 'test1',
+				'config'      => static function (Kernel $kernel) {
+					$kernel->addExtraConfig(dirname(__DIR__, 2) . '/config/idm_advertising.php');
+				},
+			];
+
+		return parent::bootKernel($options);
+	}
+
+	public function testRenderTwigComponent (): void
+	{
+		$tpl = '<twig:IdmAdvertising:Show:Banner network="adsense" banner="main" />';
+		/** @var Environment $twig */
+		$twig = self::getContainer()->get('twig');
+		$output = $twig->createTemplate($tpl)->render();
+
+		$expected = <<<'EXPECTED'
+<ins data-ad-client="ca-pub-XXXXXXX11XXX9" data-ad-slot="54555454" data-full-width-responsive="false" data-ad-format="auto" class="adsbygoogle" style="display:block;"></ins>
+<script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
+EXPECTED;
+
+		$this->assertSame($expected, $output);
+	}
+}
