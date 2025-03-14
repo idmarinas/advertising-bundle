e80bc6fc29fc5ed232e18267a8e505406b45502b
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-03-14 17:15:37 +0100

    test: add GenericNetworkTest for network and banner validation
    - Introduced a new test class `GenericNetworkTest` to validate the
      functionality of the `GenericNetwork` and its associated `GenericBanner`.
    - The test checks if the network and banner instances are created
      correctly and verifies the banner's name.

---
+++ tests/Configuration/GenericNetworkTest.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Copyright 2025 (C) IDMarinas - All Rights Reserved
+ *
+ * Last modified by "IDMarinas" on 14/03/2025, 17:09
+ *
+ * @project IDMarinas Advertising Bundle
+ * @see     https://github.com/idmarinas/advertising-bundle
+ *
+ * @file    GenericNetworkTest.php
+ * @date    14/03/2025
+ * @time    16:50
+ *
+ * @author  Iván Diaz Marinas (IDMarinas)
+ * @license BSD 3-Clause License
+ *
+ * @since   2.0.0
+ */
+
+namespace Idm\Bundle\Advertising\Tests\Configuration;
+
+use App\Kernel;
+use App\Provider\Banner\GenericBanner;
+use App\Provider\Network\GenericNetwork;
+use Idm\Bundle\Advertising\Provider\ProviderHub;
+use Idm\Bundle\Advertising\Tests\CreateKernelTestCaseTrait;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class GenericNetworkTest extends KernelTestCase
+{
+	use CreateKernelTestCaseTrait;
+
+	public function testNetwork ()
+	{
+		self::bootKernel([
+			'config' => static function (Kernel $kernel) {
+				$kernel->addExtraConfig(dirname(__DIR__) . '/config/config_generic_network.php');
+			},
+		]);
+
+		$provider = static::$kernel->getContainer()->get(ProviderHub::class);
+		$network = $provider->getNetwork('generic');
+
+		$this->assertInstanceOf(GenericNetwork::class, $network);
+		$banner = $network->getBanner('main');
+		$this->assertInstanceOf(GenericBanner::class, $banner);
+
+		$this->assertEquals('main', $banner->getName());
+	}
+}
