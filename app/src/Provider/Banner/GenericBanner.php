f8b3b2ddf753def26153b740b5b5888570eadf8e
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-03-14 17:14:22 +0100

    feat(banner): add GenericBanner class implementation
    - Introduced a new class `GenericBanner` in the `App\Provider\Banner`
      namespace, extending `AbstractBanner` for banner functionality.
    
    ide(config): added/updated automatically.
    
    flex(recipe): updated to latest version.

---
+++ app/src/Provider/Banner/GenericBanner.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Copyright 2025 (C) IDMarinas - All Rights Reserved
+ *
+ * Last modified by "IDMarinas" on 14/03/2025, 17:05
+ *
+ * @project IDMarinas Advertising Bundle
+ * @see     https://github.com/idmarinas/advertising-bundle
+ *
+ * @file    GenericBanner.php
+ * @date    14/03/2025
+ * @time    17:05
+ *
+ * @author  Iván Diaz Marinas (IDMarinas)
+ * @license BSD 3-Clause License
+ *
+ * @since   2.0.0
+ */
+
+namespace App\Provider\Banner;
+
+use Idm\Bundle\Advertising\Provider\Banner\AbstractBanner;
+
+class GenericBanner extends AbstractBanner {}
