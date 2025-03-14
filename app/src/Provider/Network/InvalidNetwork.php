06f59ab43b2a6c62df8550c59b4f4f1b6b9117e5
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-03-14 16:25:02 +0100

    feat(network): add InvalidNetwork class for network handling

---
+++ app/src/Provider/Network/InvalidNetwork.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Copyright 2025 (C) IDMarinas - All Rights Reserved
+ *
+ * Last modified by "IDMarinas" on 14/03/2025, 16:02
+ *
+ * @project IDMarinas Advertising Bundle
+ * @see     https://github.com/idmarinas/advertising-bundle
+ *
+ * @file    InvalidNetwork.php
+ * @date    14/03/2025
+ * @time    15:57
+ *
+ * @author  Iván Diaz Marinas (IDMarinas)
+ * @license BSD 3-Clause License
+ *
+ * @since   2.0.0
+ */
+
+namespace App\Provider\Network;
+
+class InvalidNetwork {}
