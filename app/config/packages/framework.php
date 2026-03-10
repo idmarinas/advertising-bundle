5d422b07cc2837968f67a41b5b34dab40da8acf3
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2026-03-10 21:58:15 +0100

    chore(config): consolidate framework configurations into a single file

--- app/config/packages/framework.php
+++ app/config/packages/framework.php
@@ -1,15 +1,15 @@
 <?php
 /**
- * Copyright 2024-2025 (C) IDMarinas - All Rights Reserved
+ * Copyright 2024-2026 (C) IDMarinas - All Rights Reserved
  *
- * Last modified by "IDMarinas" on 10/03/2025, 20:19
+ * Last modified by "IDMarinas" on 10/03/2026, 21:57
  *
  * @project IDMarinas Advertising Bundle
  * @see     https://github.com/idmarinas/advertising-bundle
  *
  * @file    framework.php
- * @date    07/03/2025
- * @time    15:31
+ * @date    30/12/2024
+ * @time    17:53
  *
  * @author  Iván Diaz Marinas (IDMarinas)
  * @license BSD 3-Clause License
@@ -19,16 +19,76 @@
 
 namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 
-use Symfony\Config\FrameworkConfig;
-
-return static function (FrameworkConfig $config) {
-    $config
-        ->secret('test')
-        ->test(true)
-        ->httpMethodOverride(false)
-        ->handleAllThrowables(true)
-    ;
-    $config->form()->enabled(false);
-    $config->propertyAccess()->enabled(true);
-    $config->phpErrors()->log(true);
+return static function (ContainerConfigurator $container) {
+	$container->extension('framework', [
+		'secret'                => 'test',
+		'http_method_override'  => false,
+		'test'                  => true,
+		'default_locale'        => 'en',
+		'enabled_locales'       => ['en'],
+		'handle_all_throwables' => true,
+		'csrf_protection'       => [
+			'enabled' => false,
+		],
+		'form'                  => [
+			'enabled'         => false,
+			'csrf_protection' => [
+				'enabled' => true,
+			],
+		],
+		'http_cache'            => [
+			'enabled' => false,
+			'debug'   => true,
+		],
+		'router'                => [
+			'enabled' => true,
+			'utf8'    => true,
+		],
+		'session'               => [
+			'enabled'            => false,
+			'handler_id'         => null,
+			'cookie_secure'      => true,
+			'cookie_samesite'    => 'lax',
+			'storage_factory_id' => 'session.storage.factory.mock_file',
+		],
+		'validation'            => [
+			'enabled'                  => false,
+			'email_validation_mode'    => 'html5',
+			'not_compromised_password' => [
+				'enabled' => false,
+			],
+		],
+		'property_access'       => [
+			'enabled' => true,
+		],
+		'php_errors'            => [
+			'log' => true,
+		],
+		'messenger'             => [
+			'enabled'    => false,
+			'routing'    => [
+				'Symfony\Component\Mailer\Messenger\SendEmailMessage' => [
+					'senders' => ['sync'],
+				],
+			],
+			'transports' => [
+				'sync' => 'in-memory://',
+			],
+		],
+		'mailer'                => [
+			'enabled'  => false,
+			'dsn'      => $_ENV['MAILER_DSN'] ?? 'null://null',
+			'envelope' => [
+				'sender' => 'idm_bundle@test.bundle',
+			],
+			'headers'  => [
+				'From' => 'IDMarinas Advertising Bundle <idm_bundle@test.bundle>',
+			],
+		],
+		'uid'                   => [
+			'enabled'                 => false,
+			'default_uuid_version'    => 7,
+			'time_based_uuid_version' => 7,
+		],
+	]);
 };
